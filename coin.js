9f0db753929a1bdb794fb5bb6f692e60f061e568
james-jcpt <adeboyejames147@gmail.com>
2026-02-05 00:49:40 +0100

    Create coin.js

---
+++ coin.js
@@ -0,0 +1,11 @@
+export const coins = [
+  { id: "bitcoin", name: "Bitcoin" },
+  { id: "ethereum", name: "Ethereum" },
+  { id: "solana", name: "Solana" },
+  { id: "cardano", name: "Cardano" },
+  { id: "ripple", name: "Ripple" },
+  { id: "polkadot", name: "Polkadot" },
+  { id: "dogecoin", name: "Dogecoin" },
+  { id: "litecoin", name: "Litecoin" },
+  // add remaining top 100 coins dynamically
+];
