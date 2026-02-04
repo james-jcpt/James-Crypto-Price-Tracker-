da394168864b45fce39638d0cfe78e98cb04a4a2
james-jcpt <adeboyejames147@gmail.com>
2026-02-05 00:43:25 +0100

    Delete leaderboard.js

--- leaderboard.js
+++
@@ -1,8 +0,0 @@
-const board = document.getElementById("leaderboard");
-const points = Number(localStorage.getItem("points") || 0);
-
-board.innerHTML = `
-#1 You — ${points} pts<br>
-#2 TraderX — 980 pts<br>
-#3 AlphaWhale — 870 pts
-`;
