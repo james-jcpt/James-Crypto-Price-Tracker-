a0818b80dcb0c6ddc5a0043cc88481950bac2b79
james-jcpt <adeboyejames147@gmail.com>
2026-02-05 19:15:29 +0100

    Delete notifications.js

--- notifications.js
+++
@@ -1,6 +0,0 @@
-export function setupNotifications() {
-    console.log("Notifications system ready");
-    setInterval(() => {
-        console.log("Check for market alerts...");
-    }, 60000);
-}
