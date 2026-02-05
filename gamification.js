96427f7152485c64a582e471d0e3d99f24ccf80c
james-jcpt <adeboyejames147@gmail.com>
2026-02-05 17:05:37 +0100

    Delete gamification.js

--- gamification.js
+++
@@ -1,7 +0,0 @@
-setInterval(() => {
-  let p = Number(localStorage.getItem("points")) || 0;
-  p++;
-  localStorage.setItem("points", p);
-  document.getElementById("vip-status").innerText =
-    p > 500 ? "MVP 🔥" : p > 100 ? "VIP ⭐" : "FREE";
-}, 5000);
